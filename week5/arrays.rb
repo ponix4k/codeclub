9e02bfb6a83776aeb6e93db1e9aabdbdd94a4f50
John Williams <ponix4k@gmail.com>
2016-10-27 19:46:37 +0100

    changes
    changes

--- week5/arrays.rb
+++ week5/arrays.rb
@@ -1,8 +1,8 @@
 puts "this is a array"
 array = Array.new # this is an empty array
-array.push("array")
-array << ("test")
-array << ("foo")
+array.push("boob")
+array << ("poop")
+array << ("wee")
 
 array.each { |chr| puts chr  }
 gets.chomp()
