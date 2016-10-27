e398dd4e6ba0555d5a315a159b3ab6cb24e139a5
John Williams <ponix4k@gmail.com>
2016-10-27 19:40:25 +0100

    update
    ive change blah blah blah

--- week2/calendar.rb
+++ week2/calendar.rb
@@ -1,5 +1,5 @@
 #1-dimensional array
-months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+months = ['jan','feb','mar','apr','may','jun','jul','aug','sep','oct','nov','dec']
 months.each do |month|
 
     puts month
@@ -13,30 +13,30 @@ end
 
 
 
-Jan = {Month:'Jan', Season: 'Winter', Days: '31'};
-Feb = {Month:'Feb', Season: 'Winter', Days: '28'};
-Mar = {Month:'Mar', Season: 'Spring', Days: '31'};
-Apr = {Month:'Apr', Season: 'Spring', Days: '30'};
-May = {Month:'May', Season: 'Spring', Days: '31'};
-Jun = {Month:'Jun', Season: 'Summer', Days: '30'};
-Jul = {Month:'Jul', Season: 'Summer', Days: '31'};
-Aug = {Month:'Aug', Season: 'Summer', Days: '31'};
-Sep = {Month:'Sep', Season: 'Autumn', Days: '30'};
-Oct = {Month:'Oct', Season: 'Autumn', Days: '31'};
-Nov = {Month:'Nov', Season: 'Autumn', Days: '30'};
-Dec = {Month:'Dec', Season: 'Winter', Days: '31'};
+jan = {Month:'Jan', Season: 'Winter', Days: '31'};
+feb = {Month:'Feb', Season: 'Winter', Days: '28'};
+mar = {Month:'Mar', Season: 'Spring', Days: '31'};
+apr = {Month:'Apr', Season: 'Spring', Days: '30'};
+may = {Month:'May', Season: 'Spring', Days: '31'};
+jun = {Month:'Jun', Season: 'Summer', Days: '30'};
+jul = {Month:'Jul', Season: 'Summer', Days: '31'};
+aug = {Month:'Aug', Season: 'Summer', Days: '31'};
+sep = {Month:'Sep', Season: 'Autumn', Days: '30'};
+oct = {Month:'Oct', Season: 'Autumn', Days: '31'};
+nov = {Month:'Nov', Season: 'Autumn', Days: '30'};
+dec = {Month:'Dec', Season: 'Winter', Days: '31'};
 
 
 
-puts Jan
-puts Feb
-puts Mar
-puts Apr
-puts May
-puts Jun
-puts Jul
-puts Aug
-puts Sep
-puts Oct
-puts Nov
-puts Dec
+puts jan
+puts feb
+puts mar
+puts apr
+puts may
+puts jun
+puts jul
+puts aug
+puts sep
+puts oct
+puts nov
+puts dec
