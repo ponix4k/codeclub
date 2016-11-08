af6d646dcbd9dd5f3e73dce7e4657aad006898c6
Paul <phyushin@gmail.com>
2016-11-08 12:14:57 +0000

    week 8 (#40)
    * added homework
    
    this homework is for week begginging 17th
    
    * added week 6 homework
    
    added homework
    
    * adding all previous branches to master as we're now tracking through forks
    
    * Added initial (yes again) code club site
    
    * committing initial gh-pages
    
    * removed duplicate images folder
    
    * commit
    
    * more attempts to bring in line
    
    * welcome page
    
    * removed include that was breaking build
    
    * reinserted the blog posts
    
    * updated
    
    * Update 2016-10-17-gitting-started.md
    
    * new post
    
    * getting rid of docs folder
    
    site has now moved to gh-pages
    
    * changed readme
    
    added some code clubbers to the readme
    
    * week7 homework
    
    this one is a little tricky - we're using Hashes - blog post about
    hashes Incoming
    
    * homework submitted
    
    * uploading some VB.net code

---
+++ week8/vb/TestConsoleApp/TestConsoleApp/Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Public Class Week8_Character
+        Private p_name As String
+        Public Property Name() As String
+            Get
+                Return p_name
+            End Get
+            Set(ByVal value As String)
+                p_name = value
+            End Set
+        End Property
+
+        Public Sub PrintDetails()
+            puts("character details")
+            puts(Me.Name)
+        End Sub
+
+    End Class
+    Sub Main()
+        Dim codeclubchar = New Week8_Character()
+
+        puts("Week 8 test visual basic app")
+        puts("---------------------")
+        puts("enter character name:")
+        codeclubchar.Name = Console.ReadLine
+        codeclubchar.PrintDetails()
+        Console.ReadKey()
+    End Sub
+
+    Sub puts(output As String)
+        Console.WriteLine(output)
+    End Sub
+
+End Module
